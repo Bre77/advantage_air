668523dd4928d9ca5279d383b69d76697c1a18aa
Brett Adams <brett@ba.id.au>
2020-09-15 17:58:24 +1000

    Rename and license

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="advantageair",  # Replace with your own username
+    name="advantage_air",  # Replace with your own username
     version="0.0.1",
     author="Brett Adams",
     author_email="brett@ba.id.au",
-    description="API helper for Advantage Airs MyAir and e-zone API",
+    description="API helper for Advantage Air's MyAir and e-zone API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Bre77/advantageair",
+    url="https://github.com/Bre77/advantage_air",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
